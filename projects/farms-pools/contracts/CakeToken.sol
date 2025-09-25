83c092dba9caa1f9b9753507b1f3fc7e6d527a57
Tombin <abc.dev0205@gmail.com>
2025-09-25 16:38:02 +0900

    feat:The number of checkpoints for each account

--- projects/farms-pools/contracts/CakeToken.sol
+++ projects/farms-pools/contracts/CakeToken.sol
@@ -42,7 +42,7 @@ contract CakeToken is BEP20("PancakeSwap Token", "Cake") {
     mapping(address => uint32) public numCheckpoints;
 
     /// @dev The EIP-712 typehash for the contract's domain
-    bytes32 public constant DOMAIN_TYPEHASH =
+    bytes32 public constant DOMAIN_TYPEHASH =    
         keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
 
     /// @dev The EIP-712 typehash for the delegation struct used by the contract
